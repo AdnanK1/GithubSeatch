abcfacfff0d80dd4a3ee980d5961e75ae38eace9
AdnanK1 <adnangitonga@gmail.com>
2022-04-16 14:12:08 +0300

    Generated component navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { DisplayComponent } from './display/display.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    DisplayComponent
+    DisplayComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
