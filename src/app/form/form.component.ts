ae85e13a2e26c4201c3aeeaea5abfff87dfb8c8f
AdnanK1 <adnangitonga@gmail.com>
2022-04-16 21:19:53 +0300

    imported formModule and implement it to the form .

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit,Output,EventEmitter } from '@angular/core';
-
+import { Data } from '../data/data';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  users:String[] = [];
+  newData = new Data("","");
+  @Output () addData = new EventEmitter<Data>();
   onSubmit(){
+    this.addData.emit(this.newData);
+    this.newData = new Data("","");
+    console.log('Data received');
 
 }
   constructor() { }
