dcf973acd689bf414ba95f9dcbaaf9fad3936223
AdnanK1 <adnangitonga@gmail.com>
2022-04-16 20:55:32 +0300

    created a class called data.

--- src/app/data-service/data-service.service.ts
+++ src/app/data-service/data-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  get Data():Observable<any> {
+    const url = "https://api.github.com/users"
+    return this.http.get<any>(`url`);
+  }
 }
